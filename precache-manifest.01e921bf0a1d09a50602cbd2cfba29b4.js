9cc7b6f048a86b0407f12ce449e2956b61697e22
doryski <dominatorskyyy@gmail.com>
2020-04-04 22:45:09 +0200

    Updates

---
+++ precache-manifest.01e921bf0a1d09a50602cbd2cfba29b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdca54899d34aa8d6dd759d02d11dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401e40a4c76cfe59806d",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "16a693a644b69275ff3b",
+    "url": "/static/css/main.425417e3.chunk.css"
+  },
+  {
+    "revision": "401e40a4c76cfe59806d",
+    "url": "/static/js/2.117801e5.chunk.js"
+  },
+  {
+    "revision": "16a693a644b69275ff3b",
+    "url": "/static/js/main.902a7640.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
